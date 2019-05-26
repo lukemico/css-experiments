c4b349b2ab3db571445b715654b72932b689e7cf
Luke Mico <lukemico@me.com>
2019-05-27 07:30:33 +1000

    implement Drag and Drop experiment

--- js/main.js
+++ js/main.js
@@ -1,15 +1,4 @@
-
-
-
-
-
 /* Zig Zag Layout */
-
-
-
-
-
-
 
 /* Image Grid */
 
@@ -21,53 +10,49 @@ var i;
 
 // Full-width images
 function one() {
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.msFlex = "100%";  // IE10
-        elements[i].style.flex = "100%";
-    }
+	for (i = 0; i < elements.length; i++) {
+		elements[i].style.msFlex = "100%"; // IE10
+		elements[i].style.flex = "100%";
+	}
 }
 
 // Two images side by side
 function two() {
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.msFlex = "50%";  // IE10
-        elements[i].style.flex = "50%";
-    }
+	for (i = 0; i < elements.length; i++) {
+		elements[i].style.msFlex = "50%"; // IE10
+		elements[i].style.flex = "50%";
+	}
 }
 
 // Four images side by side
 function four() {
-    for (i = 0; i < elements.length; i++) {
-        elements[i].style.msFlex = "25%";  // IE10
-        elements[i].style.flex = "25%";
-    }
+	for (i = 0; i < elements.length; i++) {
+		elements[i].style.msFlex = "25%"; // IE10
+		elements[i].style.flex = "25%";
+	}
 }
 
 // Add active class to the current button (highlight it)
 var header = document.getElementById("myHeader");
 var btns = header.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+	btns[i].addEventListener("click", function() {
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	});
 }
-
-
-
 
 /* Lightbox */
 
-
 // Open the Modal
 function openModal() {
-  document.getElementById('myModal').style.display = "block";
+	document.getElementById("myModal").style.display = "block";
 }
 
 // Close the Modal
 function closeModal() {
-  document.getElementById('myModal').style.display = "none";
+	document.getElementById("myModal").style.display = "none";
 }
 
 var slideIndex = 1;
@@ -75,109 +60,113 @@ showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+	showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+	showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("demo");
+	var captionText = document.getElementById("caption");
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+	captionText.innerHTML = dots[slideIndex - 1].alt;
 }
-
-
 
 /* How To Portfolio */
 
-filterSelection("all") // Execute the function and show all columns
+filterSelection("all"); // Execute the function and show all columns
 function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column-33");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
-  }
+	var x, i;
+	x = document.getElementsByClassName("column-33");
+	if (c == "all") c = "";
+	// Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+	for (i = 0; i < x.length; i++) {
+		RemoveClass(x[i], "show");
+		if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
+	}
 }
 
 // Show filtered elements
 function AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
-    }
-  }
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		if (arr1.indexOf(arr2[i]) == -1) {
+			element.className += " " + arr2[i];
+		}
+	}
 }
 
 // Hide elements that are not selected
 function RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1); 
-    }
-  }
-  element.className = arr1.join(" ");
+	var i, arr1, arr2;
+	arr1 = element.className.split(" ");
+	arr2 = name.split(" ");
+	for (i = 0; i < arr2.length; i++) {
+		while (arr1.indexOf(arr2[i]) > -1) {
+			arr1.splice(arr1.indexOf(arr2[i]), 1);
+		}
+	}
+	element.className = arr1.join(" ");
 }
 
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+	btns[i].addEventListener("click", function() {
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
+	});
 }
-
-
-
-
 
 /* Tab Gallery */
 
 function myFunction(imgs) {
-  // Get the expanded image
-  var expandImg = document.getElementById("expandedImg");
-  // Get the image text
-  var imgText = document.getElementById("imgtext");
-  // Use the same src in the expanded image as the image being clicked on from the grid
-  expandImg.src = imgs.src;
-  // Use the value of the alt attribute of the clickable image as text inside the expanded image
-  imgText.innerHTML = imgs.alt;
-  // Show the container element (hidden with CSS)
-  expandImg.parentElement.style.display = "block";
+	// Get the expanded image
+	var expandImg = document.getElementById("expandedImg");
+	// Get the image text
+	var imgText = document.getElementById("imgtext");
+	// Use the same src in the expanded image as the image being clicked on from the grid
+	expandImg.src = imgs.src;
+	// Use the value of the alt attribute of the clickable image as text inside the expanded image
+	imgText.innerHTML = imgs.alt;
+	// Show the container element (hidden with CSS)
+	expandImg.parentElement.style.display = "block";
 }
 
+/* Drag and Drop  */
 
+function allowDrop(ev) {
+	ev.preventDefault();
+}
 
+function drag(ev) {
+	ev.dataTransfer.setData("text", ev.target.id);
+}
 
-
-
-
-
-
-
+function drop(ev) {
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(data));
+}
