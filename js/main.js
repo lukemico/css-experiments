1bb3a831967a576d689f742bd6e4eaf94e2a4d94
Luke Mico <lukemico@me.com>
2019-06-05 08:10:06 +1000

    add Geolocation experiements

--- js/main.js
+++ js/main.js
@@ -170,3 +170,32 @@ function drop(ev) {
 	var data = ev.dataTransfer.getData("text");
 	ev.target.appendChild(document.getElementById(data));
 }
+
+/* Geolocation  */
+
+var x = document.getElementById("geoloc");
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		x.innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+
+function showPosition(position) {
+	x.innerHTML =
+		"Latitude: " +
+		position.coords.latitude +
+		"<br>Longitude: " +
+		position.coords.longitude;
+}
+
+// function showPosition(position) {
+//   var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+//   var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=
+//   "+latlon+"&zoom=14&size=400x300&sensor=false&key=YOUR_:KEY";
+
+//   document.getElementById("demo").innerHTML = "<img src='"+img_url+"'>";
+// }
